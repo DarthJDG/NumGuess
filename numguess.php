d811fc2cde7380b6c812216c609ca04596304ed8
Bazmega Kapa <ricsi@expattech.com>
2013-02-12 09:51:20 +0100

    Attemtpted to fix indentation issues.

--- numguess.php
+++ numguess.php
@@ -4,16 +4,16 @@
 echo "\n-={ NumGuess PHP version }=-\n\n";
 
 if ($argc < 2 || $argv[1] === '--help') {
-    echo "Usage: php numguess.php YourName Limit\n";
-    echo "Limit is optional and must be at least 10.\n\n";
-    exit;
+	echo "Usage: php numguess.php YourName Limit\n";
+	echo "Limit is optional and must be at least 10.\n\n";
+	exit;
 }
 
 $name = $argv[1];
 $defaultLimit = 10;
 $limit = isset($argv[2]) ? $argv[2] : $defaultLimit;
 if (!is_numeric($limit) || $limit < 10) {
-    $limit = $defaultLimit;
+	$limit = $defaultLimit;
 }
 echo "Hello, $name! Let's play!\n";
 
@@ -24,23 +24,23 @@ $success = false;
 $tries = 0;
 
 do {
-    echo "Your guess is: ";
-    $guess = trim(fgets(STDIN));
-    if (is_numeric($guess)) {
-	if ($guess < $mynumber) {
-	    echo "My number is bigger than that.\n";
+	echo "Your guess is: ";
+	$guess = trim(fgets(STDIN));
+	if (is_numeric($guess)) {
+		if ($guess < $mynumber) {
+			echo "My number is bigger than that.\n";
+		}
+		else if ($guess > $mynumber) {
+			echo "My number is smaller than that.\n";
+		}
+		else {
+			$success = true;
+		}
 	}
-	else if ($guess > $mynumber) {
-	    echo "My number is smaller than that.\n";
+	else {
+		echo "This is simply wrong.\n";
 	}
-        else {
-	    $success = true;
-	}
-    }
-    else {
-	echo "This is simply wrong.\n";
-    }
-    $tries++;
+	$tries++;
 } while (!$success);
 
 echo "\nYou WON!!! It took $tries tries.\n\n";
