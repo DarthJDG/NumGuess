4d73034415901afae500028046e0534a975772e4
anemgyenge <anemgyenge@gmail.com>
2013-02-16 14:08:21 +0100

    Min10 fixed + minor fixes of new lines.

--- numguess.py
+++ numguess.py
@@ -1,29 +1,31 @@
 import sys, random
 
-print "Welcome to NumGuess Python terminal version!\n\n"
-print "Enter your name: "
+print "Welcome to NumGuess Python terminal version!\n"
+print "Enter your name:"
 
 name = sys.stdin.readline().strip()
 
 if name == "":
   name = "Player"
 
-print "\nWelcome " + name + ", enter limit: "
+print "\nWelcome " + name + ", enter limit:"
 
 limit = sys.stdin.readline().strip()
 
 try:
 	limit = int(limit)
+	if limit < 10:
+		limit = 10
 except:
 	limit = 10
 
 while True:
 	tries = 0
 	number = random.randint(1, limit)
-	print "\nGuess my number between 1 and " + str(limit) + "!\n"
+	print "\nGuess my number between 1 and " + str(limit) + "!"
 
 	while True:
-		print "\nGuess: "
+		print "\nGuess:"
 
 		guess = sys.stdin.readline().strip()
 
@@ -32,15 +34,15 @@ while True:
 			guess = int(guess)
 
 			if guess < number:
-				print "Too low!"
+				print "\nToo low!"
 			elif guess > number:
-				print "Too high!"
+				print "\nToo high!"
 			else:
 				break
 		except:
 			print "\nThat was not a number, but still a try!"
 
-	print "\nWell done " + name + ", you guessed my number from " + str(tries) + " tries!\n"
+	print "\nWell done " + name + ", you guessed my number from " + str(tries) + " tries!"
 	print "Play again [Y/N]? "
 
 	again = sys.stdin.readline().strip()
