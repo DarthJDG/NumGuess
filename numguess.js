36c0daa77a3e5fbbc50240ce388e9dc94a26486a
Bazmega Kapa <ricsi@expattech.com>
2013-03-07 19:57:00 +0100

    Rewritten the JS engine according to guidelines (as far as it was possible). Renamed NumGuess to NumGuessEngine.

--- numguess.js
+++ numguess.js
@@ -1,57 +1,89 @@
 (function (glob, undefined) {
 
-	glob.NumGuess = {
+	glob.NumGuessEngine = {
 		num: null,
 		tries: null,
 		limit: null,
 		minLimit: 10,
 		name: 'Player',
+		/* override to set a different displayMethod */
+		displayMethod: function (s) { 
+			console.log(s); 
+		},
+		/* starts the game with a certain name and limit */
 		start: function (name, limit) {
-			this.name = name;
+			this.name = name || this.name;
 			this.limit = limit >= this.minLimit ? limit : this.minLimit;
+			this.show('Welcome to NumGuess Javascript version!');
 			this.restart();
 		},
-		guess: function (num) {
-			if (this.num === null) {
-				this.show('Please start the game first.');
+		/* attempt a guess, returns true/false based on success */
+		guess: function (guess) {
+			if (!/^[1-9]\d*$/.test(guess)) {
+				this.show('That\'s just plain wrong.');
+				return false;
+			}
+			
+			if (guess > this.limit) {
+				this.show('Out of range.');
+				return false;
+			}
+
+			this.tries += 1;
+			
+			if (this.num == guess) {
+				this.show('Well done ' 
+					+ this.name 
+					+ ', you guessed my number from ' 
+					+ this.tries 
+					+ (this.tries > 1 ? ' tries' : ' try')
+					+ '!');
+				this.show(this.generateCustomMessage(this.tries, this.limit));
+				this.finish();
+				return true;
 			}
 			else {
-				this.tries += 1;
-				if (this.num > num) {
-					this.show('My number is bigger than ' + num + '!');
-				}
-				else if (this.num < num) {
-					this.show('My number is smaller than ' + num + '!');
-				}
-				else {
-					this.show('Congratulations! My number is exactly ' + num + '!');
-					this.finish();
-					return true;
-				}
-			}
-			return false;
+				this.show(this.num < guess ? 'Too high!' : 'Too low!');	
+				return false;
+			}		
 		},
+		/* stop the game, reset to starting position */
 		finish: function () {
-			this.show('You had ' + this.tries + ' tries!');
 			this.tries = null;
 			this.num = null;
 		},
+		/* zeroes tries, find out random number and shows limit */
 		restart: function () {
 			this.tries = 0;
 			this.num = Math.floor(Math.random() * this.limit + 1);
-			this.show('The game has started, ' + this.name + '!'
-				+ ' I have chosen my number. It is between 1 and ' + this.limit + '.'
-				+ ' Guess it if you can!');
+			this.show('Guess my number between 1 and ' + this.limit + '!');
 		},
+		/* shows a string using displayMethod */
 		show: function (s) {
-			console.log(s);
+			this.displayMethod(s);
 		},
-		help: function () {
-			this.show(
-				'NumGuess.start(\'YourName\', MaxNumber): Start the game \n'
-				+ 'NumGuess.guess(Number): Guess the number \n'
-				+ 'NumGuess.restart(): Restart with the same name and max number \n'
-			);
+		/* returns a custom message based on tries and limit */
+		generateCustomMessage: function (tries, limit) {
+			var maxJustifiedTries = Math.floor(Math.log(limit) / Math.log(2)) + 1;
+			var messages = [
+				"You're one lucky bastard!",
+				"You are the master of this game!",
+				"You are a machine!",
+				"Very good result!",
+				"Try harder, you can do better!",
+				"I find your lack of skill disturbing!"
+			];
+			var checks = [
+				function () { return tries === 1; },
+				function () { return tries < maxJustifiedTries; },
+				function () { return tries === maxJustifiedTries; },
+				function () { return tries <= maxJustifiedTries * 1.1; },
+				function () { return tries <= limit; }
+			];
+			for (var i = 0, l = checks.length; i < l; i++) {
+				if (checks[i]()) return messages[i];
+			}
+			return messages[l];
 		}
 	};
 
