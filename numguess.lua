5eb661bc349b2c27a6d980fb776eb7d6bc38dbea
anemgyenge <anemgyenge@gmail.com>
2013-03-24 19:38:13 +0100

    Adjusted to guidelines.

--- numguess.lua
+++ numguess.lua
@@ -1,16 +1,18 @@
 theSeed = os.time()
 math.randomseed(theSeed)
 
+math.random(1, 10) -- Burn first random number, workaround for same number bug.
+
 print("Welcome to NumGuess Lua terminal version!\n")
-print("Enter your name:")
+print("Enter your name: ")
 
 name = io.read()
 
 if name == "" then
-  name = "Player"
+	name = "Player"
 end
 
-print("\nWelcome " .. name .. ", enter limit:")
+print("\nWelcome " .. name .. ", enter limit: ")
 
 limit = io.read()
 
@@ -31,40 +33,55 @@ while true do
 	print("\nGuess my number between 1 and " .. limit .. "!")
 
 	while true do
-		print("\nGuess:")
+		print("\nGuess: ")
 
 		guess = tonumber(io.read())
-
-		tries = tries + 1
 
 		if not tonumber(guess) then
 			print("\nThat was not a number, but still a try!")
 		else
 			guess = tonumber(guess)
-
-			if guess < number then
-				print("\nToo low!")
-			elseif guess > number then
-				print("\nToo high!")
+			if guess > limit or guess < 1 then
+				print("\nOut of range.")
 			else
-				break
+				if guess < number then
+					tries = tries + 1
+					print("\nToo low!")
+				elseif guess > number then
+					tries = tries + 1
+					print("\nToo high!")
+				else
+					tries = tries + 1
+					break
+				end
 			end
 		end
 	end
 
-	print("\nWell done " .. name .. ", you guessed my number from " .. tries .. " tries! " .. max_tries .. " guesses were needed.")
-
-	if tries == max_tries then
-		print "You are a machine!"
-	elseif tries > max_tries and tries <= max_tries * 1.1 then
-		print "Very good result!"
-	elseif tries > max_tries * 1.1 then
-		print "Try harder, you can do better!"
-	else
-		print "You are the master of this game!"
+	tries_declination = "try"
+	if tries > 1 then
+		tries_declination = "tries"
 	end
-	
-	print("Play again [Y/N]?")
+
+	print("\nWell done " .. name .. ", you guessed my number from " .. tries .. " " .. tries_declination .. "!")
+
+	custom_message = ""
+	if tries == 1 then
+		custom_message = "You're one lucky bastard!"
+	elseif tries == max_tries then
+		custom_message = "You are a machine!"
+	elseif tries > max_tries and tries <= max_tries * 1.1 then
+		custom_message = "Very good result!"
+	elseif tries > max_tries * 1.1 and tries <= limit then
+		custom_message = "Try harder, you can do better!"
+	elseif tries > limit then
+		custom_message = "I find your lack of skill disturbing!"
+	else
+		custom_message = "You are the master of this game!"
+	end
+
+	print(custom_message)
+	print("Play again [Y/N]? ")
 	again = io.read()
 
 	if string.upper(again) ~= 'Y' then
