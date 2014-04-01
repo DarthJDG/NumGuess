b0dde8eac1355cfb9b3a7e0f4d9f7183145d155f
Robert Biro <darthjdg@gmail.com>
2014-04-01 15:05:26 +0100

    Clean up C# code a bit
    Made it a bit more concise, tweaked a few bits here and there.

--- numguess.cs
+++ numguess.cs
@@ -6,109 +6,80 @@ namespace numguess
 	{
 		public static void Main(string[] args)
 		{
-			int limit;
-			int tries;
-			int guess;
-			string name;
+			int limit, tries, number, guess;
+			string name, message;
+			Random random = new Random();
 
-			Console.WriteLine("Welcome to NumGuess C# version!\n");
-
-			Console.Write("Enter your name: ");
-
+			Console.Write("Welcome to NumGuess C# version!\n\nEnter your name: ");
 			name = Console.ReadLine();
-
-			if (name == "") {
-				name = "Player";
-			}
+			if (name.Length == 0) name = "Player";
 
 			Console.Write("\nWelcome " + name + ", enter upper limit: ");
 
-			string limit_str = Console.ReadLine();
-
-			try {
-				limit = Convert.ToInt32(limit_str);
-
-				if (limit < 10) {
-					limit = 10;
-				}
+			try
+			{
+				limit = int.Parse(Console.ReadLine());
+				if (limit < 10) limit = 10;
 			}
-			catch {
+			catch
+			{
 				limit = 10;
 			}
 
-			int max_tries = Convert.ToInt32(Math.Floor(Math.Log(limit) / Math.Log(2)) + 1);
+			int max_tries = (int)Math.Floor(Math.Log(limit) / Math.Log(2)) + 1;
 
-			while(true) {
+			do
+			{
 				tries = 0;
-
-				Random random = new Random();
-				int number = random.Next(1, limit + 1);
+				number = random.Next(1, limit + 1);
 
 				Console.WriteLine("\nGuess my number between 1 and " + limit + "!\n");
 
-				while(true) {
+				do
+				{
 					Console.Write("Guess: ");
 
-					string guess_str = Console.ReadLine();
-
-					try {
-						guess = Convert.ToInt32(guess_str);
-
-						if (guess > limit || guess < 1) {
+					try
+					{
+						guess = int.Parse(Console.ReadLine());
+						if (guess > limit || guess < 1)
+						{
 							Console.WriteLine("Out of range.");
 						}
-						else {
-							if (guess < number) {
-								tries++;
-								Console.WriteLine("Too low!");
-							}
-							else if (guess > number) {
-								tries++;
-								Console.WriteLine("Too high!");
-							}
-							else {
-								tries++;
-								break;
-							}
+						else
+						{
+							tries++;
+							if (guess < number) Console.WriteLine("Too low!");
+							else if (guess > number) Console.WriteLine("Too high!");
 						}
 					}
-					catch {
+					catch
+					{
+						guess = 0;
 						Console.WriteLine("That's just plain wrong.");
 					}
-				}
+
+				} while (guess != number);
 
 				Console.WriteLine("\nWell done " + name + ", you guessed my number from " + tries + " " + (tries == 1 ? "try" : "tries") + "!");
 
-				String custom_message = "";
+				if (tries == 1)
+					message = "You're one lucky bastard!";
+				else if (tries < max_tries)
+					message = "You are the master of this game!";
+				else if (tries == max_tries)
+					message = "You are a machine!";
+				else if (tries <= max_tries * 1.1)
+					message = "Very good result!";
+				else if (tries <= limit)
+					message = "Try harder, you can do better!";
+				else
+					message = "I find your lack of skill disturbing!";
 
-				if (tries == 1) {
-					custom_message = "You're one lucky bastard!";
-				}
-				else if (tries == max_tries) {
-					custom_message = "You are a machine!";
-				}
-				else if (tries > max_tries && tries <= max_tries * 1.1) {
-					custom_message = "Very good result!";
-				}
-				else if (tries > max_tries * 1.1 && tries <= limit) {
-					custom_message = "Try harder, you can do better!";
-				}
-				else if (tries > limit) {
-					custom_message = "I find your lack of skill disturbing!";
-				}
-				else {
-					custom_message = "You are the master of this game!";
-				}
-
-				Console.WriteLine(custom_message);
+				Console.WriteLine(message);
 				Console.Write("Play again [y/N]? ");
 
-				string again = Console.ReadLine();
-
-				if(again.ToUpper().CompareTo("Y") != 0) {
-					break;
-				}
-			}
+			} while (Console.ReadLine().ToUpper().Equals("Y"));
 
 			Console.WriteLine("\nOkay, bye.");
 		}
