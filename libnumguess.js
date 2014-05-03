9afebfc80f5f149603a9d86eb6550a650b80ce37
Robert Biro <darthjdg@gmail.com>
2014-05-03 23:57:36 +0100

    JS: Reformat code and trim trailing spaces

--- libnumguess.js
+++ libnumguess.js
@@ -1,4 +1,4 @@
-(function (global) {
+(function(global) {
 
 	global.NumGuessEngine = {
 		num: 0,
@@ -6,61 +6,67 @@
 		limit: 0,
 		minLimit: 10,
 		name: 'Player',
+
 		/* override to set a different displayMethod */
-		displayMethod: function (s) { 
-			console.log(s); 
+		displayMethod: function(s) {
+			console.log(s);
 		},
-		welcome: function () {
+
+		welcome: function() {
 			this.show('Welcome to NumGuess Javascript version!');
 		},
+
 		/* starts the game with a certain name and limit */
-		start: function (name, limit) {
+		start: function(name, limit) {
 			limit = parseInt(limit, 10);
 			this.name = name || this.name;
 			this.limit = limit >= this.minLimit ? limit : this.minLimit;
 			this.restart();
 		},
+
 		/* attempt a guess, returns true/false based on success */
-		guess: function (guess) {
-			if (!/^[1-9]\d*$/.test(guess)) {
+		guess: function(guess) {
+			if(!/^[1-9]\d*$/.test(guess)) {
 				this.show('That\'s just plain wrong.');
 				return false;
 			}
-			
-			if (guess > this.limit) {
+
+			if(guess > this.limit) {
 				this.show('Out of range.');
 				return false;
 			}
 
 			this.tries += 1;
-			
-			if (this.num == guess) {
-				this.show('Well done ' 
-					+ this.name 
-					+ ', you guessed my number from ' 
-					+ this.tries 
+
+			if(this.num == guess) {
+				this.show('Well done '
+					+ this.name
+					+ ', you guessed my number from '
+					+ this.tries
 					+ (this.tries > 1 ? ' tries' : ' try')
 					+ '!');
 				this.show(this.generateCustomMessage(this.tries, this.limit));
 				return true;
-			}
-			else {
-				this.show(this.num < guess ? 'Too high!' : 'Too low!');	
+			} else {
+				this.show(this.num < guess ? 'Too high!' : 'Too low!');
 				return false;
-			}		
+			}
 		},
+
 		/* zeroes tries, find out random number and shows limit */
-		restart: function () {
+		restart: function() {
 			this.tries = 0;
 			this.num = Math.floor(Math.random() * this.limit + 1);
 			this.show('Guess my number between 1 and ' + this.limit + '!');
 		},
+
 		/* shows a string using displayMethod */
-		show: function (s) {
+		show: function(s) {
 			this.displayMethod(s);
 		},
+
 		/* returns a custom message based on tries and limit */
-		generateCustomMessage: function (tries, limit) {
+		generateCustomMessage: function(tries, limit) {
 			var maxJustifiedTries = Math.floor(Math.log(limit) / Math.log(2)) + 1;
 			var messages = [
 				"You're one lucky bastard!",
@@ -77,8 +83,8 @@
 				function () { return tries <= maxJustifiedTries * 1.1; },
 				function () { return tries <= limit; }
 			];
-			for (var i = 0, l = checks.length; i < l; i++) {
-				if (checks[i]()) return messages[i];
+			for(var i = 0, l = checks.length; i < l; i++) {
+				if(checks[i]()) return messages[i];
 			}
 			return messages[l];
 		}
