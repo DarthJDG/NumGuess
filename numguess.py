4f7ff945892c5c97a3b9d2e9004808112a2ed27f
Robert Biro <darthjdg@gmail.com>
2015-01-03 00:53:12 +0000

    Fix code to support Python 3.x

--- numguess.py
+++ numguess.py
@@ -1,14 +1,16 @@
+from __future__ import print_function
+
 import sys, random, math
 
-print "Welcome to NumGuess Python terminal version!\n"
-print "Enter your name: ",
+print("Welcome to NumGuess Python terminal version!\n")
+print("Enter your name: ", end="", flush=True)
 
 name = sys.stdin.readline().strip()
 
 if name == "":
 	name = "Player"
 
-print "\nWelcome " + name + ", enter limit: ",
+print("\nWelcome " + name + ", enter limit: ", end="", flush=True)
 
 limit = sys.stdin.readline().strip()
 
@@ -24,34 +26,34 @@ max_tries = math.floor(math.log(limit, 2)) + 1
 while True:
 	tries = 0
 	number = random.randint(1, limit)
-	print "\nGuess my number between 1 and " + str(limit) + "!\n"
+	print("\nGuess my number between 1 and " + str(limit) + "!\n")
 
 	while True:
-		print "Guess: ",
+		print("Guess: ", end="", flush=True)
 
 		guess = sys.stdin.readline().strip()
 
 		try:
 			guess = int(guess)
 			if guess > limit or guess < 1:
-				print "\bOut of range."
+				print("\bOut of range.")
 			else:
 				if guess < number:
 					tries = tries + 1
-					print "\bToo low!"
+					print("\bToo low!")
 				elif guess > number:
 					tries = tries + 1
-					print "\bToo high!"
+					print("\bToo high!")
 				else:
 					tries = tries + 1
 					break
 		except:
-			print "\bThat's just plain wrong."
+			print("\bThat's just plain wrong.")
 
 	tries_declination = "try"
 	if tries > 1:
 		tries_declination = "tries"
-	print "\nWell done " + name + ", you guessed my number from " + str(tries) + " " + tries_declination + "!"
+	print("\nWell done " + name + ", you guessed my number from " + str(tries) + " " + tries_declination + "!")
 
 	custom_message = ""
 
@@ -68,12 +70,12 @@ while True:
 	else:
 		custom_message = "You are the master of this game!"
 
-	print custom_message
-	print "Play again [y/N]? ",
+	print(custom_message)
+	print("Play again [y/N]? ", end="", flush=True)
 
 	again = sys.stdin.readline().strip()
 
 	if again.upper() != 'Y':
 		break
 
-print "\nOkay, bye."
+print("\nOkay, bye.")
